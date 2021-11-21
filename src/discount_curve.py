97a6485b5ff5062f623d75c494d6b459cb61f0a5
Aleksandra Zh <aleksandra260497@gmail.com>
2021-11-21 19:33:35 +0300

    Empty class

---
+++ src/discount_curve.py
@@ -0,0 +1,10 @@
+class DiscountCurve:
+    """Input: startDate, EndDate.
+    Output: rate
+    """
+
+ ##   @staticmethod
+ ##   def getRate(x_list: list, y_list: list, z: float):
+        
+        
+ ##       return answer
